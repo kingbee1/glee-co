bd31f3872b19e7d1b785ff3025dfce6470c1a155
Abiodun <abiodunhodonu@gmail.com>
2022-08-22 01:07:09 +0100

    fixed cart error

--- pages/components/Cart.js
+++ pages/components/Cart.js
@@ -7,9 +7,53 @@ import { useStateContext } from '../../context/StateContext'
 import { urlFor } from '../../lib/Client'
 
 const Cart = () => {
+  const cartRef = useRef();
+  const { totaltPrice, totalQuantities, setShowCart, cartItems } = useStateContext();
+
   return (
-    <div>
-      see stt
+    <div className='cart-wrapper' ref={cartRef}>
+      <div className='cart-container'>
+        <button type='button' className='cart-heading'onClick={()=>setShowCart(false)}>
+          <AiOutlineLeft />
+          <span className='heading'>Your Cart</span>
+          <span className='cart-num-items'>({totalQuantities} items)</span>
+        </button>
+
+        {cartItems.length < 1 && (
+          <div className='empty-cart'>
+            <AiOutlineShopping size={150} />
+            <h3>Fill up your cart</h3>
+            <Link href='/'>
+              <button type='button' onClick={()=>setShowCart(false)} className='btn'>Keep shopping</button>
+            </Link>
+          </div>
+        )}
+        <div className='product-container'>
+          {cartItems.length >= 1 && cartItems.map((item) =>(
+            <div className='product' key={item._id}>
+              <img src={urlFor(item?.image[0])} className="cart-product-image" />
+              <div className='item-desc'>
+                <div className='flex top'>
+                  <h5>{item.name}</h5>
+                  <h4>₦{item.price}</h4>
+                </div>
+
+                <div className='flex bottom'>
+                  <div>
+                    <p className='quantity-desc'>
+                      <span className='minus' onClick={''}><AiOutlineMinus /></span>
+                      <span className='num' onClick={''}>0</span>
+                      <span className='plus' onClick={''}><AiOutlinePlus /></span>
+                    </p>
+                  </div>
+                </div>
+
+              </div>
+            </div>
+          ))}
+        </div>
+        
+      </div>
     </div>
   )
 }
