10e41deb55acee63c98083e65d4b94b8db5cbb5b
Abiodun <abiodunhodonu@gmail.com>
2022-08-27 23:43:47 +0100

    payment integration

--- pages/api/Stripe.js
+++ pages/api/Stripe.js
@@ -1,15 +1,35 @@
-import Stripe from "stripe";
-
-const stripe = new Stripe(process.env.NEXT_PUBLIC_STRIPE_SECRET_KEY);
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
 export default async function handler(req, res) {
-    if(req.method === 'POST') {
-        try {
-            
-        } catch (error) {
-            res.status(500).json({
-                statusCode: 500, message: error.message
-            })
-        }
+  if (req.method === 'POST') {
+    console.log(req.body.cartItems)
+   
+    try {
+        const params = {
+            submit_type: 'pay',
+            mode: 'payment',
+            payment_method_types: ['card'],
+            billing_address_collection: 'auto',
+            shipping_options: [
+                {shipping_rate: 'shr_1LbXDUKoUlyhO5NmUm2AGL46'},
+                {shipping_rate: 'shr_1LbXF2KoUlyhO5NmxtxbDkCH'}
+            ],
+            line_items: req.body.cartItems.map((item) => {
+                const img = item.image[0]
+            }),
+            mode: 'payment',
+            success_url: `${req.headers.origin}/?success=true`,
+            cancel_url: `${req.headers.origin}/?canceled=true`,
+          }
+
+      // Create Checkout Sessions from body params.
+      const session = await stripe.checkout.sessions.create(params);
+      res.redirect(303, session.url);
+    } catch (err) {
+      res.status(err.statusCode || 500).json(err.message);
     }
+  } else {
+    res.setHeader('Allow', 'POST');
+    res.status(405).end('Method Not Allowed');
+  }
 }
--- pages/components/Cart.js
+++ pages/components/Cart.js
@@ -5,10 +5,30 @@ import { TiDeleteOutline }  from 'react-icons/ti'
 import { toast } from 'react-hot-toast'
 import { useStateContext } from '../../context/StateContext'
 import { urlFor } from '../../lib/Client'
+import getStripe from '../../lib/getStripe'
 
 const Cart = () => {
   const cartRef = useRef();
   const { totalPrice, totalQuantities, setShowCart, cartItems, toggleCartItemQuantity, onRemove } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe()
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type' : 'application/json',
+      },
+      body: JSON.stringify(cartItems),
+    });
+    if (response.statusCode === 500) return;
+
+    const data = await response.json()
+
+    toast.loading('Redirecting...');
+
+    stripe.redirectToCheckout({ sessionId: data.id })
+  }
 
   return (
     <div className='cart-wrapper' ref={cartRef}>
@@ -64,7 +84,7 @@ const Cart = () => {
               <h3>₦{totalPrice}</h3> 
             </div>
             <div className='btn-container'>
-              <button type='button' className='btn'>Pay with your card</button>
+              <button type='button' className='btn' onClick={handleCheckout} >Pay with your card</button>
             </div>
           </div>
         )}
